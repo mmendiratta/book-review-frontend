a945f6b08262ea859451e813adc6a630cf40bc82
mmendiratta <mannimendiratta@yahoo.com>
2023-01-22 14:55:00 -0600

    hash router

--- src/sections/App.js
+++ src/sections/App.js
@@ -5,7 +5,7 @@ import { Bio } from './bio/Bio';
 import { Books } from './books/Books';
 import { BookSelectionProvider } from '../BookSelectionContext';
 import { SelectedReviewSection } from './selectedReview/SelectedReviewSection';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Login } from '../login/Login';
 
 const HomePage = () => {
@@ -26,13 +26,10 @@ const LoginPage = () => {
 
 const App = () => {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
-    </BrowserRouter>
-     
   );
 };
 export default App;
